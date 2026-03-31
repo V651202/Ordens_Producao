dcdde112cd79b30b48a46cd4d8e85209505807a6
V651202 <victor_g_kostetzer@estudante.sesisenai.org.br>
2026-03-31 08:38:43 -0300

    Commit inicial - ambiente configurado e limpo

---
+++ database.py
@@ -0,0 +1,48 @@
+# Database > Creation and configuration of SQLite database
+import sqlite3
+
+# Constant with name of the database
+# The file will be created during the first execution
+db_order = 'ordens.bd'
+
+def get_connection():
+    '''
+    Create and return a connection with SQLite database
+    
+    The property row_factory allows the access of the columns by name
+    (Ex: order['product'] instead of index (Ex: order[1]))
+    
+    Return:
+        sqlite3.Connection: Connection object with database
+    '''
+    
+    conn = sqlite3.connect(db_order)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    '''
+    Initialize database creating a table named 'orders' if does not exists. 
+    Safe to call multiple times.
+    '''
+    
+    conn = get_connection()
+
+    # Cursor allows the execution of SQL commands
+    cursor = conn.cursor()
+
+    # 'IF NOT EXISTS' ensures the command don't fails if the table exists
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    status TEXT DEFAULT 'Pendent',
+    created_at TEXT DEFAULT (datetime('now', 'localtime'))
+    )
+    ''')
+
+    # commit() Saves changes in .db file
+    conn.commit()
+    conn.close()
+    print("Database successfully initialized!")

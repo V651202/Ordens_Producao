bb946aa923247c139a7731722ab7cfa80fbbae1e
Victor Gabriel Kostetzer <victor_g_kostetzer@estudante.sesisenai.org.br>
2026-04-01 10:16:18 -0300

    Create router POST

--- app.py
+++ app.py
@@ -1,58 +1,142 @@
-# Back_end FLASK > API Rest's Routes
+#BACK-END FLASK: ROTAS DA API REST
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database import init_db, get_connection
 
-# Create an Flash's application instance
+#Cria uma instância da aplicação Flash
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-# Habilitate CORS
+#Habilitar os CORS
 CORS(app)
 
-# Route Nº1 - Initial screen
+#ROTA N1 - PÁGINA INICIAL ------------------------------------------
 @app.route('/')
-
 def index():
-    #FEED INDEX.HTML FILE IN STATIC FOLDER
+    #ALIMENTAR O ARQUIVO INDEX.HTML DA PASTA STATIC
     return app.send_static_file('index.html')
-
-# Route Nº2 - API STATUS
+#ROTA N2 - STATUS API
 @app.route('/status')
 def status():
-    '''
-    API's VERIFICATION ROUTE (HEALTH)
-    Returns JSON informing  server's active state
-    '''
-
+    """ROTA DE VERIFICAÇÃO DA API(SAÚDE)
+    RETORNAR UM JSON INFORMANDO QUE O SERVIDOR ESTA 
+    ATIVO"""
     return jsonify({
         "status": "online",
-        "system": "Order System's Production",
-        "version": "1.0.0",
-        "message": "Hello Factory, API working!"
+        "sistema": "Sistema de ordem de Produção",
+        "versao":"1.0.0",
+        "mensagem":"Ola, Fabrica, API FUNCIONANDO!"
     })
-
-#Route Nº3 - List all orders (GET)
-@app.route('/orders', methods=['GET'])
-
-def list_orders():
-    '''
-    LIST ALL REGISTER EDF PRODUCTION ORDERS
-    METHODS HTTP: GET
-    URL: https://localhost:5000/orders
-    Returns: Orders and list in JSON format
-    '''
-    
+#ROTA N3 - LISTAR TODAS AS ORDENS(GET) -----------------------------
+@app.route('/ordens', methods=['GET'])
+def listar_ordens():
+    """
+    LISTAR TODAS AS ORDENS DE PRODUÇÃO CADASTRADAS.
+    MÉTODOS HTTP: GET
+    URL: http://localhost:5000/ordens
+    Retorna: Lista e ordens em formato JSON.
+    """
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM orders ORDER BY id DESC')
-    orders = cursor.fetchall()
+    cursor.execute('SELECT * FROM ordens ORDER BY id DESC')
+    ordens = cursor.fetchall()
+    conn.close()
+    #Converte cada Row do SQLite em discionario Python para serializar em JSON    
+    return jsonify([dict(o) for o in ordens])
+
+# ROTA POR ID - BUSCAR UMA ORDEM ESPECIFICA PELO ID(GET) -----------------
+
+@app.route('/ordens/<int:ordem_id>', methods=['GET'])
+
+def buscar_ordem(ordem_id):
+    """
+    Buscar uma única ordem de produção pelo ID.
+    
+    Parametros de URL:
+        - ordem id(int): ID da ordem a ser buscada.
+        
+    Retornar:
+        200 + JSON da ordem, se for encontrada.
+        404 + mensagem de erro, se não existir.
+    """
+    conn = get_connection()
+    cursor = conn.cursor()
+    #o '?' é substituido pelo valor de ordem_id de forma segura
+    cursor.execute('SELECT * FROM ordens WHERE id = ?', (ordem_id,))
+    ordem = cursor.fetchone()  #ele rotorna um único registro ou None
     conn.close()
     
-    # Converts every SQLite Row in Python dictionary to serialize in JSOn
-    return jsonify([dict(o) for o in orders])
+    if ordem is None:
+        return jsonify({'erro': f'Ordem {ordem_id} nao encontarada.'}), 404
+    return jsonify(dict(ordem)), 200
 
-# STARTING POINT
-if __name__== '__main__':
+
+# ROTA : CRIAR NOVA ORDEM DE PRODUÇÃO (POST)----------------------------
+@app.route('/ordens', methods=['POST'])
+def criar_ordem():
+    """
+    Cria uma nova ordem de produção a partir dos dados JSON enviados.
+    
+    Body esperado(JSON):
+    
+        produto      (str) : Nome do produto     - Obrigatório
+        quantidade   (int) : Quantidade de peças - Obrigatório, > 0
+        status       (str) : Opcional            - Padrão : 'Pendente'
+    
+    Retorno:
+        201 : JSON da ordem criada, em caso de sucesso.
+        400 : mensagem de erro, se dados inválidos
+    """
+
+    dados = request.get_json()
+    
+    if not dados:
+        return jsonify({'erro': 'Body da requisicao ausente ou invalido'}), 400
+    
+    #verificação de campo obrigatório (produto)
+    produto = dados.get('produto', '').strip()
+    if not produto:
+        return jsonify({'erro': 'Campo "Produto" e obrigatorio e nao pode ser vazio.'}), 400
+    #verificação de campo obrigatório (quantidade)
+    quantidade = dados.get('quantidade')
+    if quantidade is None:
+        return jsonify({'erro': 'Campo "quantidade" e obrigatorio.'}), 400
+    #Verifica se a quantidade é um número inteiro e positivo
+    try:
+        quantidade = int(quantidade)
+        if quantidade <= 0:
+            raise ValueError()
+    except (ValueError, TypeError):
+        return jsonify({'erro': 'Campo "Quantidade" deve ser um numero inteiro e positivo.'}), 400
+    #Status (*pendente, em andamento, concluída) - opcional
+    status_validos = ['Pendente','Em andamento','Concluida']
+    status = dados.get('status', 'Pendente')
+    if status not in status_validos:
+        return jsonify({'erro': f'Status invalido. Use {status_validos}'}), 400
+    #Inserção dos dados no banco
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        'INSERT INTO ordens (produto, quantidade, status) VALUES(?, ?, ?)',
+        (produto, quantidade, status)
+    )
+    conn.commit()
+    
+    #Recuperando o ID gerado automaticamente pelo banco
+    novo_id = cursor.lastrowid
+    conn.close()
+    
+    #Buscar o registros que foi recém-criado
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM ordens WHERE id = ?', (novo_id,))
+    nova_ordem = cursor.fetchone()
+    conn.close()
+    #201 - Retornar "created"
+    return jsonify(dict(nova_ordem)), 201
+    
+#PONTO DE PARTIDA -------------------------------------------------------
+
+if __name__=='__main__':
     init_db()
     
     app.run(debug=True, host='0.0.0.0', port=5000)

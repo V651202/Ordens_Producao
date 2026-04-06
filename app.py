f3e8813cd145036dcdda57a06857b1539f5613cd
Victor Gabriel Kostetzer <victor_g_kostetzer@estudante.sesisenai.org.br>
2026-04-06 14:04:54 -0300

    Create route DELETE

--- app.py
+++ app.py
@@ -171,25 +171,60 @@ def atualizar_ordens(ordem_id):
     conn = get_connection()
     cursor = conn.cursor()
     
-    cursor.execute('SELECT id FROM ordens WHERE id = ?', (ordem_id))
+    cursor.execute('SELECT id FROM ordens WHERE id = ?', (ordem_id,))
     if cursor.fetchone() is None:
         conn.close()
         return jsonify({'erro': f'Ordem {ordem_id} nao encontrada.'}), 404
     
     #De fato atualizando a execução
-    cursor.execute('UPDATE ordens SET status = ? WHERE id = ?', (novo_status, ordem_id))
+    cursor.execute('UPDATE ordens SET status = ? WHERE id = ?', (novo_status, ordem_id,))
     
     conn.commit()
     conn.close() 
     
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM ordens WHERE id = ?', (ordem_id))
+    cursor.execute('SELECT * FROM ordens WHERE id = ?', (ordem_id),)
     ordem_atualizada = cursor.fetchone()
     conn.close()
     
     return jsonify(dict(ordem_atualizada)), 200
 
+#ROTA - REMOVER UMA ORDEM (DELETE)
+@app.route('/ordens/int:ordem_id>', methods=['DELETE'])
+def remover_ordem(ordem_id):
+    """
+    Remover permanentemente uma ordem de producao pelo ID.
+    
+    Parâmetros de URL:
+        ordem_id (int) : ID a ordem a ser removida.
+        
+    Retorna:
+        200 - confirmacao (mensagem)
+        400 - erro (mensagem) se a ordem nao for encontrada
+    """
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    #verificação de existência antes de DELETAR o registro
+    cursor.execute('SELECT id, produto FROM ordens WHERE id = ?', (ordem_id),)
+    ordem = cursor.fetchone()
+    
+    if ordem is None:
+        conn.close()
+        return jsonify({'erro': f'ordem de numero {ordem_id} nao encontrada.'}), 404
+    
+    #variavel que guarda o nome do produto apagado para ser usado posteriormente na mensagem de confirmação.
+    nome_produto_apagado = ordem['produto']
+    
+    #execução de fato da operação
+    cursor.execute('DELETE FROM ordens WHERE id = ?', (ordem_id,))
+    conn.commit()
+    conn.close()
+    
+    return jsonify({'mensagem' : f'Ordem de numero {ordem_id} ({nome_produto_apagado}) removida com sucesso!', 'id_removido' :
+                    ordem_id}), 200
+    
 #PONTO DE PARTIDA -------------------------------------------------------
 
 if __name__=='__main__':
